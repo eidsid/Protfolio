d55597e99489ebbd7e12da6ee7e41b54c66b969f
eid sid <eidsiddiedis@gmail.com>
2021-11-12 14:45:06 +0200

    add About me section

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,16 +21,15 @@ window.addEventListener('scroll', () => {
     let scrollPosition = document.documentElement.scrollTop;
     // make loop in sections and get the position of it and controll classs
     sections.forEach((section) => {
-        if (
-            scrollPosition >= section.offsetTop -
-            section.offsetHeight * 0.5 &&
-            scrollPosition < section.offsetTop * 1.1
-        ) {
+        section.classList.remove('active');
+        if (scrollPosition >= section.offsetTop - section.offsetHeight * 0.9) {
             // get the id of section add add active class to link 
             let currentId = section.attributes.id.value;
+            section.classList.add('active');
             linkStat(currentId);
         }
     });
+    scrolltop(scrollPosition);
 
 })
 
@@ -38,23 +37,31 @@ window.addEventListener('scroll', () => {
 let linkStat = (id) => {
     asid.querySelectorAll('li').forEach((link) => {
         link.classList.remove('active');
+
     });
+
+
     let linkSelector = document.querySelector(` .sidebare ul li a[href="#${id}"]`);
     linkSelector.parentElement.classList.add("active");
 
-
 }
 
+document.querySelector('.scrollDown').addEventListener('click', () => {
+    document.documentElement.scrollTop = document.querySelector('#home').offsetHeight + 35;
+
+})
+
+scrolltop = (scrollPosition) => {
+
+    let section1 = document.querySelector('#home').offsetHeight,
+        scrolltop = document.querySelector('.top');
+    // add stacky for nave bar and 
+    if (scrollPosition > section1.offsetTop + 500) {
+        scrolltop.classList.add("active");
+    } else {
+        scrolltop.classList.remove("active");
+
+    }
 
 
-// document.querySelector('.top').addEventListener('click', () => {
-//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// });
-
-// let section1 = ""
-//     // add stacky for nave bar and 
-// if (scrollPosition > section1.offsetTop + 300) {
-
-// } else {
-
-// }
+}
